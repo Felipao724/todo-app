2d873b20ca20185f7c0459bc163969892c03104b
Felipe Salas <felipe_jesus724@hotmail.com>
2022-07-24 11:09:00 -0600

    se arreglaron problemas con el localStorage y se agregaron los filtros

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { Todo, TodoList } from './classes';
+import { TodoList } from './classes';
 import { crearTodoHtml } from './js/componentes';
 import './styles.css';
 
 export const todoList = new TodoList();
 
 todoList.todos.forEach(crearTodoHtml);
-
-const newTodo = new Todo('Aprender JavaScript')
-todoList.nuevoTodo(newTodo);
 
 console.log('todos', todoList.todos);
 
